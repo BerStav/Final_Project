eace3e79e3f55cfc3447c23046c56a72c1ae8ce1
Vitaly Bersenev <vitalion.bersenev@gmail.com>
2023-01-06 02:32:27 +0700

    SelectionArray method done!

---
+++ Task/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача написать программу, которая из имеющегося массива строк, 
+// длина которых меньше либо равна 3м символам. 
+// Первоначальный массив задается на старте выполнения алгоритма. 
+// При решении задачи используются только массивы, без использования коллекций.
+
+string[] arrayOrig = new string[5] { "hi", "Russia", "135", "1", "hello" };
+string[] arrayFinal = new string[arrayOrig.Length];
+
+void SelectionArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = arrayOrig[i];
+            count++;
+        }
+    }
+}
